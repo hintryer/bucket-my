f8095f1158580a007a865309c565632767b16a48
hintryer <gindeny@gmail.com>
2026-06-26 12:14:41 +0800

    更新 Bili23downloader 的下载链接，添加 FFmpeg 安装，优化环境配置脚本

--- setupmy.ps1
+++ setupmy.ps1
@@ -1,11 +1,15 @@
 # setup-my-env.ps1
+scoop bucket rm main
+scoop bucket add main https://mirror.nju.edu.cn/git/scoop-main.git
 
-# 安装常用工具
+scoop bucket rm extras
+scoop bucket add extras https://mirror.nju.edu.cn/git/scoop-extras.git
+
+# 安装My常用工具
 scoop install My/7zip
 scoop install My/Git
 scoop install Aria2
 scoop config aria2-warning-enabled false
-scoop install My/FFmpeg
 scoop install My/Steamcommunity
 scoop install My/EmEditor
 scoop install My/Bili23downloader
@@ -19,8 +23,29 @@ scoop install My/GeekUninstaller
 scoop install My/Chrome-Plus
 scoop install My/Chrome
 scoop install My/Mp3tag
+scoop install My/FFmpeg
 scoop install My/lx-music-desktop
 
+# 安装My常用软件
+scoop install My/7zip
+scoop install My/Git
+scoop install Aria2
+scoop config aria2-warning-enabled false
+scoop install My/Steamcommunity
+scoop install My/EmEditor
+scoop install Bili23downloader  #main与extras 没有这个软件，使用My仓库
+scoop install Clash-Verge-Rev
+scoop install Lanzouyun         #main与extras 没有这个软件，使用My仓库
+scoop install LocalSend
+scoop install Notepad4
+scoop install UMI-OCR
+scoop install FSViewer
+scoop install GeekUninstaller
+scoop install Chrome-Plus
+scoop install Chrome
+scoop install Mp3tag
+scoop install FFmpeg
+scoop install lx-music-desktop
 # 清理旧版本
 scoop cleanup *
 
